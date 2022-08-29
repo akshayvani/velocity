05d8c8b8120102fbf22be72afdaeea16061dd574
HP <HP@DESKTOP-MHU59BL>
2022-08-29 21:33:45 +0530

    [ak] 1st programe

---
+++ src/velocity1/example1.java
@@ -0,0 +1,9 @@
+package velocity1;
+
+public class example1 
+{
+           public static void main(String[] args)
+           {
+			    System.out.println("23rd april start the testing class");
+		}
+}
